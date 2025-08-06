8f2840e9d4dea24d153fb08e0802eb1266989651
Nikky Kumar <knikki7505@gmail.com>
2025-08-06 12:25:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 function validEmail(str) {
   //your JS code here.
+	let original = str
+	if(original == ""){
+		return false;
+	}
+	if(original.includes("@")){
+		return true;
+	} 
+	if(original[0] == "@"){
+		return false
+	}
+	if(original.includes(".com")  || original.includes(".com.in") || original.includes(".in") ){
+		return true
+	} else{
+		return false;
+	}
 }
 
 // Do not change the code below.
